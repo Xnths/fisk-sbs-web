ba73509c3d6dc301a05ec359404438a49e0d144b
xnths <jonathascastilhoes@gmail.com>
2021-06-01 07:01:54 -0300

    Stablishing POST and GET metT method

--- app/hightlights.js
+++ app/hightlights.js
@@ -6,6 +6,16 @@ const bulletinBoard = document.getElementById('bulletin-board');
 
 const xhr = new XMLHttpRequest();
 
+function dateFormat(date) {
+    let regex = /\d{1,4}-\d{1,2}-\d{1,2}/gm
+    let selectedDate = date.match(regex)[0];
+
+    let splitedDate = selectedDate.split('-');
+    let dayMonthYear = `${splitedDate[2]}/${splitedDate[1]}/${splitedDate[0]}`
+
+    return dayMonthYear;
+}
+
 function createNotes() {
     bulletinBoard.innerHTML = "";
 
@@ -15,36 +25,44 @@ function createNotes() {
     if (!notes.length > 0) bulletinBoard.innerHTML = "<p class='bulletin-board__empty'>There is no notes</p>";
 
     notes.forEach(note => {
-        const li = document.createElement("li");
+        const div = document.createElement("div");
         const h3 = document.createElement("h3");
         const p1 = document.createElement("p");
         const p2 = document.createElement("p");
         const p3 = document.createElement("p");
+        const p4 = document.createElement("p");
         const a1 = document.createElement("a");
         const a2 = document.createElement("a");
 
-        li.classList.add("bulletin-board__item");
+        div.classList.add("bulletin-board__item");
         h3.classList.add("bulletin-board__item-title");
         p1.classList.add("bulletin-board__item-message");
         p2.classList.add("bulletin-board__item-author");
         p3.classList.add("bulletin-board__item-createdAt");
+        a1.classList.add("bulletin-board__item-action");
+        a2.classList.add("bulletin-board__item-action");
+        p4.classList.add("bulletin-board__item-options");
         a1.classList.add("bulletin-board__item-edit");
         a2.classList.add("bulletin-board__item-delete");
 
         h3.innerHTML = note.title;
         p1.innerHTML = note.message;
         p2.innerHTML = note.author;
-        p3.innerHTML = note.createdAt;
+        p3.innerHTML = dateFormat(note.createdAt);
+
         a1.setAttribute("href", "#");
         a1.innerHTML = "Edit";
         a2.setAttribute("href", "#");
         a2.innerHTML = "Delete";
 
-        [h3, p1, p2, p3, a1, a2].forEach(element => {
-            li.appendChild(element);
+        p4.appendChild(a1);
+        p4.appendChild(a2);
+
+        [h3, p1, p2, p3, p4].forEach(element => {
+            div.appendChild(element);
         })
 
-        bulletinBoard.appendChild(li);
+        bulletinBoard.appendChild(div);
     })
 }
 

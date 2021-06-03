59248d585e5334e91335ad64b0d2b4524449f942
xnths <jonathascastilhoes@gmail.com>
2021-06-03 14:32:00 -0300

    updating highlights

--- app/hightlights.js
+++ app/hightlights.js
@@ -1,10 +1,23 @@
-const txt_title = document.getElementById('title');
-const txt_message = document.getElementById('message');
-const txt_author = document.getElementById('author');
-const btn_post = document.getElementById('post');
+const form = document.querySelector('form');
 const bulletinBoard = document.getElementById('bulletin-board');
 
-const xhr = new XMLHttpRequest();
+bulletinBoard.addEventListener('click', event => {
+    const isEdit = event.target.className === "bulletin-board__item-action bulletin-board__item-edit";
+    const isDelete = event.target.className === "bulletin-board__item-action bulletin-board__item-delete";
+
+    if (isDelete) {
+        const note = event.target.closest('[data-id]');
+        const id = note.dataset.id;
+
+        fetch(`http://localhost:3000/api/bulletinboard/${id}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => updateBulletinBoard())
+            .catch(error => alert(error));
+    }
+})
 
 function dateFormat(date) {
     let regex = /\d{1,4}-\d{1,2}-\d{1,2}/gm
@@ -16,90 +29,66 @@ function dateFormat(date) {
     return dayMonthYear;
 }
 
-function createNotes() {
+function createNotes(notes) {
     bulletinBoard.innerHTML = "";
-
-    let response = xhr.response;
-    let notes = JSON.parse(response);
 
     if (!notes.length > 0) bulletinBoard.innerHTML = "<p class='bulletin-board__empty'>There is no notes</p>";
 
     notes.forEach(note => {
         const div = document.createElement("div");
-        const h3 = document.createElement("h3");
-        const p1 = document.createElement("p");
-        const p2 = document.createElement("p");
-        const p3 = document.createElement("p");
-        const p4 = document.createElement("p");
-        const a1 = document.createElement("a");
-        const a2 = document.createElement("a");
-
         div.classList.add("bulletin-board__item");
-        h3.classList.add("bulletin-board__item-title");
-        p1.classList.add("bulletin-board__item-message");
-        p2.classList.add("bulletin-board__item-author");
-        p3.classList.add("bulletin-board__item-createdAt");
-        a1.classList.add("bulletin-board__item-action");
-        a2.classList.add("bulletin-board__item-action");
-        p4.classList.add("bulletin-board__item-options");
-        a1.classList.add("bulletin-board__item-edit");
-        a2.classList.add("bulletin-board__item-delete");
 
-        h3.innerHTML = note.title;
-        p1.innerHTML = note.message;
-        p2.innerHTML = note.author;
-        p3.innerHTML = dateFormat(note.createdAt);
+        const html = `
+            <h3 class="bulletin-board__item-title">${note.title}</h3>
+            <p class="bulletin-board__item-message">${note.message}</p>
+            <p class="bulletin-board__item-author">${note.author}</p>
+            <p class="bulletin-board__item-createdAt">${dateFormat(note.createdAt)}</p>
+            <p class="bulletin-board__item-options">
+                <a class="bulletin-board__item-action bulletin-board__item-edit" href="#">Edit</a>
+                <a class="bulletin-board__item-action bulletin-board__item-delete" href="#">Delete</a>
+            </p>
+        `
 
-        a1.setAttribute("href", "#");
-        a1.innerHTML = "Edit";
-        a2.setAttribute("href", "#");
-        a2.innerHTML = "Delete";
-
-        p4.appendChild(a1);
-        p4.appendChild(a2);
-
-        [h3, p1, p2, p3, p4].forEach(element => {
-            div.appendChild(element);
-        })
+        div.innerHTML = html;
+        div.dataset.id = note.id;
 
         bulletinBoard.appendChild(div);
     })
 }
 
 async function updateBulletinBoard() {
-    xhr.open('GET', 'http://localhost:3000/api/bulletinboard');
+    fetch(`http://localhost:3000/api/bulletinboard`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        const listPromise = response.json();
 
-    xhr.send();
-
-    xhr.onload = () => createNotes();
+        listPromise.then(list => createNotes(list));
+    }).catch(error => {
+        alert(error);
+    })
 }
 
-btn_post.addEventListener('click', (event) => {
+form.addEventListener('submit', event => {
     event.preventDefault();
 
-    const title = txt_title.value;
-    const message = txt_message.value;
-    const author = txt_author.value;
-
-    const data = JSON.stringify({
-        author: author,
-        title: title,
-        message: message
+    fetch(`http://localhost:3000/api/bulletinboard`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            author: event.target.querySelector('[data-author]').value,
+            title: event.target.querySelector('[data-title]').value,
+            message: event.target.querySelector('[data-message]').value
+        })
+    }).then(() => {
+        updateBulletinBoard();
+    }).catch(error => {
+        alert(error);
     })
-
-    xhr.open('POST', 'http://localhost:3000/api/bulletinboard', true);
-
-    xhr.send(data);
-
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            if (xhr.status == 201) {
-                updateBulletinBoard();
-            } else {
-                console.log(xhr.responseText);
-            }
-        }
-    }
 })
 
 updateBulletinBoard();

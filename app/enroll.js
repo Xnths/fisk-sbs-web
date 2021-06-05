481bdcf6b8692299b1a91cfe6e47c02c52161d80
xnths <jonathascastilhoes@gmail.com>
2021-06-05 18:20:51 -0300

    Implementing automatic search for CEP

--- app/enroll.js
+++ app/enroll.js
@@ -1,4 +1,4 @@
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll('.input');
 
 inputs.forEach(input => {
     input.addEventListener('blur', event => {
@@ -48,7 +48,9 @@ const errorMessage = {
         valueMissing: "The cellphone field cannot be empty."
     },
     CEP: {
-        valueMissing: "The CEP field cannot be empty."
+        valueMissing: "The CEP field cannot be empty.",
+        patternMismatch: "CEP is invalid.",
+        customError: "It wasn't possible to retrive the location data."
     },
     city: {
         valueMissing: "The city field cannot be empty."
@@ -75,7 +77,39 @@ function validate(input) {
 }
 
 const validation = {
-    birthday: input => verifyAge(input)
+    birthday: input => verifyAge(input),
+    CEP: input => retriveCEP(input)
+}
+
+function fillAddressFields(data) {
+    const city = document.querySelector('[data-type="city"]');
+    const neighborhood = document.querySelector('[data-type="neighborhood"]');
+
+    city.value = data.localidade;
+    neighborhood.value = data.bairro;
+}
+
+function retriveCEP(input) {
+    const cep = input.value.replace(/\D/g, '');
+
+    if (!input.validity.patternMismatch && !input.validity.valueMissing) {
+        fetch(`https://viacep.com.br/ws/${cep}/json`, {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.erro) {
+                    input.setCustomValidity("It wasn't possible to retrive the location data.")
+                    return;
+                }
+                input.setCustomValidity('');
+                fillAddressFields(data);
+            });
+    }
 }
 
 function verifyAge(input) {
